668b1ed904880d4e0249e9356ebd1ed5b2ec50ee
joelcoxokc <joelcox@hisimagination.com>
2015-02-20 03:13:41 -0800

    Update: Postfix route view models with -demo

--- src/demo/app.js
+++ src/demo/app.js
@@ -28,11 +28,11 @@ export class App {
     this.router.configure(config => {
       config.title = 'Aurelia Interface';
       config.map([
-        { route: ['', 'welcome'],  moduleId: 'welcome'     , title: 'ai', toolbar:this.toolbar },
-        { route: 'buttons-demo' ,  moduleId: 'buttons-demo', title: 'ai-Btn', nav: true, toolbar:this.toolbar },
-        { route: 'tabs-demo'    ,  moduleId: 'tabs-demo'   , title: 'ai-Tabs', nav: true , toolbar:this.toolbar},
-        { route: 'components'   ,  moduleId: 'components'  , title: 'ai-Components', nav: true , toolbar:this.toolbar},
-        { route: 'layouts'      ,  moduleId: 'layouts'     , title: 'ai-Layouts'   , nav: true , toolbar:this.toolbar},
+        { route: ['', 'welcome'],  moduleId: 'welcome'          , title: 'ai', toolbar:this.toolbar },
+        { route: 'buttons-demo' ,  moduleId: 'buttons-demo'     , title: 'ai-Btn', nav: true, toolbar:this.toolbar },
+        { route: 'tabs-demo'    ,  moduleId: 'tabs-demo'        , title: 'ai-Tabs', nav: true , toolbar:this.toolbar},
+        { route: 'components'   ,  moduleId: 'components-demo'  , title: 'ai-Components', nav: true , toolbar:this.toolbar},
+        { route: 'layouts'      ,  moduleId: 'layouts-demo'     , title: 'ai-Layouts'   , nav: true , toolbar:this.toolbar},
       ]);
     });
 
