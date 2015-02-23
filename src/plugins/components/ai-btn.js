2b7b6d78ec52eac2d0ca845c046133b9e22affcb
joelcoxokc <joelcox@hisimagination.com>
2015-02-23 12:49:50 -0800

    Update: Component: ai-btn add text & bg properties

--- src/plugins/components/ai-btn.js
+++ src/plugins/components/ai-btn.js
@@ -15,6 +15,8 @@ export class AiBtn extends AiElement{
             .withProperty('icon')
             .withProperty('size')
             .withProperty('waves')
+            .withProperty('bg')
+            .withProperty('text')
             .withProperty('nextIcon', 'nextIconChanged', 'next-icon')
 
     }
@@ -35,6 +37,16 @@ export class AiBtn extends AiElement{
         _.each(properties, (item)=>{
             this[item] && classList.push(`btn-${this[item]}`)
         })
+        if(this.text){
+            for(var color of this.text.split(' ')){
+                classList.push(`text-${color}`)
+            }
+        }
+        if(this.bg){
+            for(var color of this.bg.split(' ')){
+                classList.push(`bg-${color}`)
+            }
+        }
         this.waves && classList.push('waves-effect', `waves-${this.waves}`)
         this.addClass.apply(this, classList)
         this.icon &&( this.useIcon(this.icon) )
