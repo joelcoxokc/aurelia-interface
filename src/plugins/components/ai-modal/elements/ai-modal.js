9f96a5df7752893162ffa12b8105fc8546730748
joelcoxokc <joelcox@hisimagination.com>
2015-02-26 05:20:53 -0800

    Create component(ai-modal)

---
+++ src/plugins/components/ai-modal/elements/ai-modal.js
@@ -0,0 +1,36 @@
+import {Behavior} from 'aurelia-templating'
+import {AiElement} from './ai-element'
+
+export class AiModal extends AiElement{
+
+    static metadata(){
+
+        return Behavior
+            .customElement('ai-modal')
+            .withProperty('size')
+            .withProperty('drop')
+
+    }
+
+    static inject(){
+
+        return [Element]
+
+    }
+
+
+    constructor(element) {
+
+        this.element = element
+
+    }
+
+
+    bind(){
+
+        this.addClass('ai-modal', `modal-${this.size}`)
+
+    }
+
+}
+
