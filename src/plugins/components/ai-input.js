684c2ce1b50e665eac049d5630cbef2de7a7bd6b
joelcoxokc <joelcox@hisimagination.com>
2015-02-26 01:01:18 +0000

    Update: component(ai-input) rebuilding

--- src/plugins/components/ai-input.js
+++ src/plugins/components/ai-input.js
@@ -10,6 +10,7 @@ export class AiInputAttachedBehavior{
 				x.withProperty('color');
 				x.withProperty('label');
 				x.withProperty('active');
+				x.withProperty('icon');
 			})
 			.useView('./ai-input.html')
 	}
@@ -19,6 +20,7 @@ export class AiInputAttachedBehavior{
 	constructor(element){
 		this.element = element;
 		this.border  = 'blue'
+		this.containers = {}
 		
 	}
 
@@ -30,6 +32,8 @@ export class AiInputAttachedBehavior{
 			evt.preventDefault();
 			this.active = true;
 		})
+		this.icon && this.createIcon();
+		this.prefix && this.createContainer('prefix', this.prefix)
 		this.bindInput();
 	}
 	
@@ -39,7 +43,7 @@ export class AiInputAttachedBehavior{
 
 	bindInput(){
 		this.label ? this.createLabel() : this.findLabel()
-		
+		this.icon  ? this.createIcon()  : this.findIcon()
 		this.input.classList.add('input-control');
 		this.input.addEventListener('focus', this.toggleFocus(true));
 		this.input.addEventListener('focusout', this.toggleFocus(false));
@@ -54,7 +58,6 @@ export class AiInputAttachedBehavior{
 		
 	}
 	
-
 	toggleFocus(value){
 		var _this = this;
 		return function(evt){
@@ -81,6 +84,15 @@ export class AiInputAttachedBehavior{
 
 	}
 	
+	createContainer(container, content){
+		var name = container ? container : 'input'
+		
+		this.containers[name] = document.createElement('DIV')
+		this.containers[name].classList.add(`input-${container ? container+'-': ''}container`);
+		content && (this.containers[name].appendChild(content))
+	}
+	
+	// get label byTagName if exists addClass .input-label
 	findLabel(){
 		var labelEl = this.element.getElementsByTagName('label')[0]
 		if(labelEl){
@@ -89,6 +101,24 @@ export class AiInputAttachedBehavior{
 		}
 	}
 	
+	// get icon byTagName if exists addClass .input-icon
+	findIcon(){
+		var iconEl = this.element.getElementsByTagName('I')[0]
+		if(iconEl){
+			this.iconEl = iconEL;
+			this.iconEl.classList.add('input-icon');
+		}
+	}
+	
+	// create new Icon element, icon.addClass .input-icon, append to element, element.addClass has-icon
+	createIcon(){
+		this.iconEl = document.createElement('I')
+		this.iconEl.classList.add(this.icon, 'input-icon')
+		this.element.appendChild(this.iconEl)
+		this.element.classList.add('has-icon')
+	}
+	
+	// create new Label element, label.addClass .input-label, set innerHTMl to this.label, append to element
 	createLabel(){
 		this.labelEl = document.createElement('LABEL')
 		this.labelEl.classList.add('input-label')
@@ -96,10 +126,10 @@ export class AiInputAttachedBehavior{
 		this.element.appendChild(this.labelEl)
 	}
 	
+	// create new Border element, border.addClass .input-border, append to element
 	createBorder(){
 		this.borderEl = document.createElement('DIV')
 		this.borderEl.classList.add('input-border')
 		this.element.appendChild(this.borderEl)
 	}
-
 }
