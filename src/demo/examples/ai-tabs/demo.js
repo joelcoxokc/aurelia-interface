0452d6a08701cf7fe0c9f94c96fa4b5a1cddf2ec
joelcoxokc <joelcox@hisimagination.com>
2015-02-25 23:42:37 -0800

    Update: Create ai-tabs demo

---
+++ src/demo/examples/ai-tabs/demo.js
@@ -0,0 +1,24 @@
+export class TabDemo {
+
+    constructor () {
+        this.selectedTabRef = 'tab-2'
+    }
+
+    activate(params, queryString, config) {
+
+    }
+
+    updateTabSliderPosition (tabsInstance, next) {
+        let slider = tabsInstance.element.querySelector('.ai-tab-slider')
+        let sliderWidth = 100 / tabsInstance.tabLinks.length
+
+        slider.style.width = sliderWidth + 10 + '%';
+        slider.style.left = tabsInstance.activeTabLink.offsetLeft + 'px';
+
+        setTimeout(function(){
+            slider.style.width = sliderWidth + '%';
+        }, 200)
+
+        next()
+    }
+}
